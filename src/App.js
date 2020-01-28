afe855d4b4f69a26e29b52498674dea80573d6ba
nicholasc861 <31120213+nicholasc861@users.noreply.github.com>
2020-01-27 23:35:41 -0500

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import Typing from 'react-typing-animation';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="background">
-        <background>
-          <Typing loop={false} speed={20}>
-            <div className="title">
-              Hi! I'm <span style={{fontWeight: "bolder"}}>Nicholas Chung-Hun</span>!
-            </div>
-          </Typing>
-        </background>
-      </div>
+      <header class="hero">
+        <div class="hero-content">
+          <h1 class="hero-header">
+            Nicholas Chung-Hun
+          </h1>
+          <p className="subtitle">
+            Computer Engineering Student at the<br>
+            </br>University of Waterloo
+          </p>
+          <div class="github-icon">
+            <a href="https://github.com/nicholasc861">
+              <svg class="github"></svg>
+            </a>
+          </div>
+          <div class="devpost-icon">
+            <a href="https://devpost.com/nicholasc861">
+              <svg class="devpost"></svg>
+            </a>
+          </div>
+        </div>
+      </header>
     );
   }
 }
