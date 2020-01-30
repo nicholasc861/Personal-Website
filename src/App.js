3a5b356bb14e178709c1ec746cd183c2cff07ce2
nicholasc861 <31120213+nicholasc861@users.noreply.github.com>
2020-01-29 21:00:33 -0500

    progress

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import TextLoop from 'react-text-loop';
 import './App.css';
+
+const nameStyle = {
+  color: '#7EB77F',
+  fontWeight: '600',
+};
 
 class App extends Component {
   render() {
@@ -8,14 +14,14 @@ class App extends Component {
       <header class="hero">
         <div class="hero-content">
           <h1 class="hero-header">
-            Nicholas Chung-Hun
+            Hello! I'm <span style={nameStyle}>Nicholas Chung-Hun.</span>
           </h1>
-          <p className="subtitle">
-            Computer Engineering Student at the<br>
+          <h2 className="subtitle">
+            Computer Engineering Student<br>
             </br>University of Waterloo
-          </p>
-          <div class="icons">
-            <ul>
+          </h2>
+          <div className="icons">
+            <ul className='social'>
             <li><a href="https://github.com/nicholasc861" target="_blank">
               <svg class="github"></svg>
             </a></li>
