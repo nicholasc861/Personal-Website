3a08d79523a7201d781105789dd892b9d762268e
Nicholas C <nicholas.chunghun@gmail.com>
2020-04-11 01:00:47 -0400

    updated

---
+++ build/precache-manifest.a52a81459f181b5b62a600ec0e234a5f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "523ef1aebb7d0612ac7fb6123d755e96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dad753ab7f0ef09858f",
+    "url": "/static/css/main.008f1050.chunk.css"
+  },
+  {
+    "revision": "590a35fd950a5ca9e833",
+    "url": "/static/js/2.ea52cb16.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.ea52cb16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dad753ab7f0ef09858f",
+    "url": "/static/js/main.699a4d39.chunk.js"
+  },
+  {
+    "revision": "eae1e96a3b2e5d56a29a",
+    "url": "/static/js/runtime-main.3c6a7099.js"
+  },
+  {
+    "revision": "47353db12e0af7877ab4e7f21ad23cc8",
+    "url": "/static/media/background.47353db1.jpg"
+  },
+  {
+    "revision": "8cf9855d29218cda737b61b1ce7fcfdd",
+    "url": "/static/media/devpost.8cf9855d.svg"
+  },
+  {
+    "revision": "17544729b5543f65ef07119e575a8beb",
+    "url": "/static/media/github.17544729.svg"
+  },
+  {
+    "revision": "65ca7b4e9958eb674b8ffbfa8cb11f5a",
+    "url": "/static/media/linkedin.65ca7b4e.svg"
+  }
+]);
