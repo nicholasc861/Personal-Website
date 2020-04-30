8e2ed3b37d4e4476aea79feb239c6de671718837
Nicholas C <nicholas.chunghun@gmail.com>
2020-04-29 23:20:21 -0400

    fixing view on different devices

---
+++ src/utils/media-queries.js
@@ -0,0 +1,13 @@
+const device = {
+    mobile: "400px",
+    tablet: "834px",
+    laptop: "1080px",
+    desktop: "2560px",
+}
+
+const contentWidth = "1440px"
+
+module.exports = {
+    device: device,
+    contentWidth: contentWidth
+}
