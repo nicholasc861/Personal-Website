a472281dc266ef661a341fcdefe532730783e107
Nicholas Chung-Hun <nicholas.chunghun@gmail.com>
2021-05-22 01:00:23 -0400

    Update contact section

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,7 +5,7 @@ import { scrollreveal, srBottomConfig } from "../utils/scrollreveal";
 import styled from "styled-components";
 
 const Contact = styled.div`
-  padding: 50px 250px 0px;
+  padding: 0px;
   background-color: #1c1d1f;
 `;
 
@@ -17,8 +17,9 @@ const Copyright = styled.div`
 
 const Section = styled.div`
   padding: 15px;
-  font-size: 14px;
+  font-size: 18px;
   color: #f7f9f9;
+  text-align: center;
 
   br {
     line-height: 10px;
@@ -48,6 +49,14 @@ const Section = styled.div`
   }
 `;
 
+const Seperator = styled.div`
+  margin: auto;
+  margin-top: 20px;
+  height: 0px;
+  width: 325px;
+  border: 1px solid #2fbf71;
+`;
+
 const ContactSection = () => {
   const reveal1 = useRef(null);
   const reveal2 = useRef(null);
@@ -64,88 +73,11 @@ const ContactSection = () => {
   return (
     <Contact id="contact">
       <Container>
-        <Row>
-          <Col xs={5}>
-            <Section ref={reveal1}>
-              <h1>
-                Summary
-                <br />
-                <div style={{ color: "#2fbf71" }}>—</div>
-              </h1>
-              Hello! My name is Nicholas Chung-Hun and I'm a Computer
-              Engineering Student at the University of Waterloo.
-              <br />
-              <br />
-              Find me on the field playing Ultimate Frisbee or browsing the
-              latest developments in technology.
-              <br />
-              <br />
-              I'm always interested in future opportunities or to grab a coffee
-              so feel free to reach out!
-            </Section>
-          </Col>
-          <Col>
-            <Section ref={reveal2}>
-              <h1>
-                Find Out More
-                <br />
-                <div style={{ color: "#2fbf71" }}>—</div>
-              </h1>
-              <h2>
-                <a href="home" target="_blank" rel="noreferrer">
-                  Resume
-                </a>
-              </h2>
-              <h2>
-                <a
-                  href="https://www.github.com/nicholasc861"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Github
-                </a>
-              </h2>
-              <h2>
-                <a
-                  href="https://www.devpost.com/nicholasc861"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Devpost
-                </a>
-              </h2>
-              <h2>
-                <a
-                  href="https://www.linkedin.com/in/nicholaschunghun/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  LinkedIn
-                </a>
-              </h2>
-            </Section>
-          </Col>
-          <Col>
-            <Section ref={reveal3}>
-              <h1>
-                Get in Touch
-                <br />
-                <div style={{ color: "#2fbf71" }}>—</div>
-              </h1>
-              <h2>General Inquiries</h2>
-              <a href="mailto:contact@nicholaschunghun.com" target="_top">
-                contact@nicholaschunghun.com
-              </a>
-              <br />
-              <br />
-              <h2>Internship Opportunities</h2>
-              <a href="mailto:intern@nicholaschunghun.com" target="_top">
-                intern@nicholaschunghun.com
-              </a>
-            </Section>
-          </Col>
-        </Row>
+        <Section>
+        Get in touch at <a href="mailto:contact@nicholaschunghun.com">contact@nicholaschunghun.com</a>
+        </Section>
       </Container>
+      <Seperator />
       <Copyright ref={reveal4}>
         Developed by Nicholas Chung-Hun © 2021
       </Copyright>
